52d511d5177049e8a345a6602c06aba9d735f4bf
IncompententTrout <95280926+IncompetentTrout@users.noreply.github.com>
2022-03-31 12:42:52 +0100

    yaga ai start

--- Assets/! Jaga Game/Scripts/AI/YAGAAI.cs
+++ Assets/! Jaga Game/Scripts/AI/YAGAAI.cs
@@ -5,6 +5,36 @@ using UnityEngine.AI;
 
 public class YAGAAI : MonoBehaviour
 {
+    public NavMeshAgent agent;
+
+    public Transform player;
+
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    [Header ("Patroling")]
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    [Header ("Attacking")]
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+    [Header ("States")]
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+
+    private void Awake() 
+    {
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+    
+    private void Patroling()
+    {
+        if (!walkPointSet) SearchWalkPoint()
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +44,6 @@ public class YAGAAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        playerInSightRange = 
     }
 }
