8cb7f70e294ca68ff4004ce1dae1749169340dd8
Visertan <denissj217@gmail.com>
2022-05-18 17:05:28 +0100

    Added Sounds and music files

---
+++ Assets/! Jaga Game/Scripts/PlaySound.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{
+    public GameObject audioSource;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        
+    }
+}
