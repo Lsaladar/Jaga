985ca9b35a1132e58ad6dc219214b2f34ce7e1b6
Lsaladar <95280668+Lsaladar@users.noreply.github.com>
2022-05-20 11:45:21 +0100

    Journal

---
+++ Assets/! Jaga Game/Scripts/UI/Objectives.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Objectives : MonoBehaviour
+{
+    public List<GameObject> crossOuts = new List<GameObject>();
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
